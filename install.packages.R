9e83d48d1a63653ea3d9caeb19c9739299e38082
al2na <aakalin@gmail.com>
2015-09-23 15:40:54 +0200

    first commit

---
+++ install.packages.R
@@ -0,0 +1,10 @@
+# package list, source this file or run line by line
+
+## CRAN packages
+install.packages()
+
+## BioC packages
+source("http://bioconductor.org/biocLite.R")
+biocLite("genomation")
+
+## github packages
