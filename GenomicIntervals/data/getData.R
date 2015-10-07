beb95be1db217b2d41d76b1b98ce587ed1007e56
al2na <aakalin@gmail.com>
2015-10-08 00:46:12 +0200

    new data

--- GenomicIntervals/data/getData.R
+++ GenomicIntervals/data/getData.R
@@ -4,15 +4,20 @@ library(rtracklayer)
 bw.file="http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/foldChange/E003-H3K4me3.fc.signal.bigwig"
 k4me3=import(bw.file, 
        which=seqinfo(BigWigFile(bw.file))["chr20"])
+k4me3=k4me3[k4me3$score !=0,]
+k4me3=keepSeqlevels(k4me3,"chr20")
 
 bw.file="http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/foldChange/E003-DNase.fc.signal.bigwig"
 dnase=import(bw.file, 
              which=seqinfo(BigWigFile(bw.file))["chr20"])
+dnase=dnase[dnase$score !=0,]
+dnase=keepSeqlevels(dnase,"chr20")
 
 bw.file="http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/foldChange/E003-H3K4me1.fc.signal.bigwig"
 k4me1=import(bw.file, 
              which=seqinfo(BigWigFile(bw.file))["chr20"])
-k4me3
+k4me1=k4me1[k4me3$score !=0,]
+k4me1=keepSeqlevels(k4me1,"chr20")
 
 export.bw(k4me3,"H1.ESC.H3K4me3.chr20.bw")
 export.bw(k4me1,"H1.ESC.H3K4me1.chr20.bw")
